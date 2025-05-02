934625475d10cd3d45bf2343c7cee6ff44b70b0b
Mahfuj Hossain <108601283+mahfuj38@users.noreply.github.com>
2025-05-02 16:20:35 +0600

    Add Data/ApplicationDbContext.cs 1746181234065

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using SimpleMvcApp.Models;
+
+namespace SimpleMvcApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
